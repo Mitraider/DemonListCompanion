2ad7d435652e88b5c9e706eefc92aed29a896676
Mitraider <mitraider08lol@gmail.com>
2026-04-21 16:17:52 +0200

    Learnt how to hook a level, added a hook that detect a completion, the lv name, and the creator

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
+
+class $modify(CompanionPlayLayer, PlayLayer) {
+    void levelComplete() {
+        PlayLayer::levelComplete();
+
+        auto level = PlayLayer::get()->m_level;
+        std::string lvName = level->m_levelName;
+        std::string lvCreator = level->m_creatorName;
+
+        FLAlertLayer::create("Level complete", "You completed "+ lvName +" by "+ lvCreator+ "! gg", "Nice")->show();
+    }
+};
+
+$on_mod(Loaded) {
+    log::info("Mod chargé i guess");
+}
